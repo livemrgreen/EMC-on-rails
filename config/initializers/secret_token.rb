079fd42f58d57479311f4ea08aa907648e3aa8e3
Yannick Paz <livemrgreen@gmail.com>
2013-12-01 17:10:20 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EMCOnRails::Application.config.secret_key_base = '5f6ed75051e6c8d7e94a238771700a2864cd7b43ab32ae6c76c14306c0683296c430f8cbef68683a486ec3b59dc10dbe44483a03a0c4c8ee7b769a8d99851cd3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EMCOnRails::Application.config.session_store :cookie_store, key: '_EMC-on-rails_session'
